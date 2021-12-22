326dfc90caa8e8a95bcb3eb99744699e6dbf7dc4
Svetloslav Novoselski <svetli.novoselski@gmail.com>
2021-12-22 17:03:36 +0200

    feat(redux): setup global store

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import {Grid, Button} from 'semantic-ui-react'
+import {useSelector} from 'react-redux'
+
 
 function App() {
+    const exampleStoreData = useSelector(state => state)
+    console.log(exampleStoreData)
     return (
         <Grid divided="horizontal" stackable>
             <Grid.Row columns={2}>
                 <Grid.Column >
                     <Button basic color="red">Have fun! ;)</Button>
                 </Grid.Column>
-
                 <Grid.Column>
-                    <Button basic color="red">Have fun! :)</Button>
-                </Grid.Column>
-
-                <Grid.Column>
-                    <Button basic color="red">Have fun! :)</Button>
+                    {exampleStoreData.data}
                 </Grid.Column>
             </Grid.Row>
         </Grid>
