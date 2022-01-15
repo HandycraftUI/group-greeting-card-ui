168d581222f01ab516e6973eeff57da0aa66732c
Svetloslav Novoselski <svetli.novoselski@gmail.com>
2022-01-15 14:12:49 +0200

    feat(config): add setup routes config

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 import './index.css'
@@ -8,10 +9,13 @@ import App from './App'
 import index from './store'
 
 ReactDOM.render(
-    <Provider store={index}>
-        <React.StrictMode>
-            <App/>
-        </React.StrictMode>
-    </Provider>,
+    <BrowserRouter>
+        <Provider store={index}>
+            <React.StrictMode>
+                <App/>
+            </React.StrictMode>
+        </Provider>
+    </BrowserRouter>
+    ,
     document.getElementById('root')
 )
