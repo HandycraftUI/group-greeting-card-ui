4c2520416e7c82121da89c285a5ae3b14299beba
Svetloslav Novoselski <svetli.novoselski@gmail.com>
2022-01-15 16:37:44 +0200

    feat(config): add media queries config

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import React, {Suspense} from 'react'
 import {useRoutes} from 'react-router-dom'
 import routes from './routes.jsx'
+import styled from 'styled-components'
+import {respondTo} from './style-config/respond-to'
 
-function App() {
+//Example how to use media queries with styled-components
+const Wrapper = styled.div`
+  background-color: aqua;
+
+  ${respondTo.xs`
+        background-color: red;
+    `}
+
+  ${respondTo.sm`
+        background-color: blue;
+    `}
+
+  ${respondTo.md`
+        background-color: yellow;
+    `}
+
+  ${respondTo.lg`
+        background-color: orange;
+    `}
+`
+
+const App = () => {
     const routing = useRoutes(routes)
     return (
-        <>
+        <Wrapper>
             <Suspense fallback={<div>Loading...</div>}>
                 {routing}
             </Suspense>
-        </>
+        </Wrapper>
     )
 }
 
---
+++ src/style-config/breakpoints.js
@@ -0,0 +1,7 @@
+// Breakpoints that provide min-width of media queries
+export const breakpoints = {
+    xs: '480px',
+    sm: '768px',
+    md: '992px',
+    lg: '1200px'
+}
