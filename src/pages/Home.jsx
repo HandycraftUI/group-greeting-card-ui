c967b4784620c9251637f1be2c04cf92ceeacdd8
vladimir290702 <vladimirovichh@abv.bg>
2022-03-03 15:50:46 +0200

    feat(homepage): add media queries for first section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,38 +3,132 @@ import { MDBContainer } from 'mdb-react-ui-kit'
 import styled from 'styled-components'
 
 import CustomButton from '../components/CustomButtom/CustomButton'
+import { respondTo } from '../style-config/respond-to'
+
+const FirstSectionWrapper = styled.div`
+    ${respondTo.xsmall`
+        display: block;
+        margin-top: 2rem;
+    `}
+
+    ${respondTo.medium`
+        display: flex;
+        margin-top: 1rem;
+    `}
+`
 
 const IntroDiv = styled(MDBContainer)`
-    margin-top: 10rem;
+    ${respondTo.xsmall`
+        margin-top: 2rem;
+        text-align: center;
+    `}
+
+    ${respondTo.small`
+        text-align: left;     
+    `}
+
+    ${respondTo.medium`
+        margin-top: 6rem;
+    `}
+
+    ${respondTo.large`
+        margin-top: 10rem;  
+    `}
+`
+
+const TextContainer = styled(MDBContainer)`
+    ${respondTo.xsmall`
+        width:80%;
+    `}
+
+    ${respondTo.small`
+        width: 100%;
+    `}
 `
 
 const ButtonDiv = styled.div`
     width: 55%;
+
+    ${respondTo.xsmall`
+        width: 100%;
+        display: block;
+        text-align: center;
+    `}
+
+    ${respondTo.small`
+        width: 70%;
+        display: flex;
+        text-align: left;
+    `}
+
+    ${respondTo.medium`
+        width: 100%;
+    `}
+
+    ${respondTo.large`
+        width: 70%;
+    `}
+`
+
+const FirstButton = styled(MDBContainer)`
+    ${respondTo.xsmall`
+        margin-bottom: 1rem;
+    `}
+`
+
+const ImageDiv = styled(MDBContainer)`
+    text-align: center;
+
+    ${respondTo.xsmall`
+        margin-top: 1.5rem;
+    `}
+
+    ${respondTo.medium`
+        margin-top: 2rem;
+    `}
 `
 
 const Image = styled.img`
-    width: 750px;
-    height: 550px;
+   
+    ${respondTo.xsmall`
+        width: 320px;
+        height: 220px;
+    `}
+
+    ${respondTo.small`
+        width: 420px;
+        height: 320px;
+    `}
+
+    ${respondTo.large`
+        width: 550px;
+        height: 430px;
+    `}
+
+    ${respondTo.xlarge`
+        width: 710px;
+        height: 500px;
+    `}
 `
 
 const Home = () => {
     return (
-        <div className='d-flex p-4'>
+        <FirstSectionWrapper>
             <IntroDiv>
-                <MDBContainer>
+                <TextContainer>
                     <h2>The easiest way to send a group card.</h2>
                     <p>
                         Group cards made easy. Create an online group card
                         in 1 minute with unlimited signers.
                     </p>
-                </MDBContainer>
+                </TextContainer>
 
-                <ButtonDiv className='d-flex'>
-                    <MDBContainer>
+                <ButtonDiv>
+                    <FirstButton>
                         <CustomButton variant='primary'>
                             Start a Card
                         </CustomButton>
-                    </MDBContainer>
+                    </FirstButton>
                     <MDBContainer>
                         <CustomButton variant='outline-primary'>
                             Try the demo
@@ -43,10 +137,10 @@ const Home = () => {
                 </ButtonDiv>
             </IntroDiv>
 
-            <MDBContainer>
+            <ImageDiv>
                 <Image src="https://www.groupgreeting.com/assets/spotlight/group-greeting-card-samples-4bd14f3ae81bd0088f98f5f9411e9bb0999b3a5adc7c2b57108d15bf32ce8925.png" alt="" />
-            </MDBContainer>
-        </div>
+            </ImageDiv>
+        </FirstSectionWrapper>
     )
 }
 
