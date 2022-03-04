8f4ce7c386c62bbf06416171d92d9b9d61323492
vladimir290702 <vladimirovichh@abv.bg>
2022-03-04 21:02:49 +0200

    feat(homepage): create second section design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 
 import CustomButton from '../components/CustomButtom/CustomButton'
 import { respondTo } from '../style-config/respond-to'
+import useTheme from '../hooks/use-theme'
 
 const FirstSectionWrapper = styled.div`
     ${respondTo.xsmall`
@@ -111,36 +112,76 @@ const Image = styled.img`
     `}
 `
 
+const Card = styled(MDBContainer)`
+    text-align: center;
+    border: 3px solid ${({ theme }) => theme.palette.tertiary};
+`
+
+const ParagraphContainer = styled(MDBContainer)`
+    padding: 0 3rem;
+`
+
 const Home = () => {
+    const theme = useTheme()
+
     return (
-        <FirstSectionWrapper>
-            <IntroDiv>
-                <TextContainer>
-                    <h2>The easiest way to send a group card.</h2>
-                    <p>
-                        Group cards made easy. Create an online group card
-                        in 1 minute with unlimited signers.
-                    </p>
-                </TextContainer>
+        <>
+            <FirstSectionWrapper>
+                <IntroDiv>
+                    <TextContainer>
+                        <h2>The easiest way to send a group card.</h2>
+                        <p>
+                            Group cards made easy. Create an online group card
+                            in 1 minute with unlimited signers.
+                        </p>
+                    </TextContainer>
 
-                <ButtonDiv>
-                    <FirstButton>
-                        <CustomButton variant='primary'>
-                            Start a Card
-                        </CustomButton>
-                    </FirstButton>
-                    <MDBContainer>
-                        <CustomButton variant='outline-primary'>
-                            Try the demo
-                        </CustomButton>
-                    </MDBContainer>
-                </ButtonDiv>
-            </IntroDiv>
+                    <ButtonDiv>
+                        <FirstButton>
+                            <CustomButton variant='primary'>
+                                Start a Card
+                            </CustomButton>
+                        </FirstButton>
+                        <MDBContainer>
+                            <CustomButton variant='outline-primary'>
+                                Try the demo
+                            </CustomButton>
+                        </MDBContainer>
+                    </ButtonDiv>
+                </IntroDiv>
 
-            <ImageDiv>
-                <Image src="https://www.groupgreeting.com/assets/spotlight/group-greeting-card-samples-4bd14f3ae81bd0088f98f5f9411e9bb0999b3a5adc7c2b57108d15bf32ce8925.png" alt="" />
-            </ImageDiv>
-        </FirstSectionWrapper>
+                <ImageDiv>
+                    <Image src="https://www.groupgreeting.com/assets/spotlight/group-greeting-card-samples-4bd14f3ae81bd0088f98f5f9411e9bb0999b3a5adc7c2b57108d15bf32ce8925.png" />
+                </ImageDiv>
+            </FirstSectionWrapper>
+
+            <MDBContainer className='mt-5 d-block d-sm-flex py-3' theme={theme}>
+                <Card className='py-3 me-3' theme={theme}>
+                    <h5>1</h5>
+                    <img src="https://storage.googleapis.com/groupgreeting/assets/images/graphics/dusk/svg/thumbnails.svg" />
+                    <h2>Choose a Card</h2>
+                    <ParagraphContainer>
+                        <p>Select a card model for any occation including Birthdays, Farewell and many more.</p>
+                    </ParagraphContainer>
+                </Card>
+                <Card className='py-3 me-3' theme={theme}>
+                    <h5>2</h5>
+                    <img src="https://storage.googleapis.com/groupgreeting/assets/images/graphics/dusk/svg/email.svg" />
+                    <h2>Ask People to Sign</h2>
+                    <ParagraphContainer>
+                        <p>Send the card link to friends so they cant also sign the card.</p>
+                    </ParagraphContainer>
+                </Card>
+                <Card className='py-3 me-3' theme={theme}>
+                    <h5>3</h5>
+                    <img src="https://storage.googleapis.com/groupgreeting/assets/images/graphics/dusk/svg/approval.svg " />
+                    <h2>Card is Sent</h2>
+                    <ParagraphContainer>
+                        <p>The card is sent to the recipient via email at the time and date selected.</p>
+                    </ParagraphContainer>
+                </Card>
+            </MDBContainer>
+        </>
     )
 }
 
