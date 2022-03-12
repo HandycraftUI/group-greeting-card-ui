a8f7535b82bf35cf2d986a9425c894d9b2ee1f33
vladimir290702 <vladimirovichh@abv.bg>
2022-03-12 15:51:07 +0200

    feat(profile): update design

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 import { MDBContainer, MDBInput } from 'mdb-react-ui-kit'
-import { BsPersonCircle } from 'react-icons/bs'
-import { MdOutlineSettings } from 'react-icons/md'
+import { MdOutlineSettings, MdModeEditOutline } from 'react-icons/md'
 import { useSelector } from 'react-redux'
 
 import CustomButton from '../CustomButtom/CustomButton'
 import useTheme from '../../hooks/use-theme'
 
 const Delimeter = styled(MDBContainer)`
-   height: 2px;
+   height: 3px;
    border-radius: 50%;
    background-color: ${({ theme }) => theme.palette.navbar.primary};
+`
+
+const Image = styled.img`
+   width: 400px;
+   height: 360px;
+   border-radius: 50%;
 `
 
 const Profile = () => {
@@ -26,33 +31,35 @@ const Profile = () => {
 
     return (
         <MDBContainer>
-            <MDBContainer className='d-flex'>
-                <MDBContainer>
-                    <MDBContainer>
-                        <BsPersonCircle style={{ fontSize: '250px' }} />
-                    </MDBContainer>
-                    <MDBContainer>
+            <MDBContainer className='d-flex py-5'>
+                <MDBContainer className='text-center'>
+                    <div>
+                        <MDBContainer>
+                            <Image src="https://icon-library.com/images/no-profile-picture-icon/no-profile-picture-icon-2.jpg" />
+                        </MDBContainer>
+                    </div>
+                    <div className='mt-3'>
                         <h3>Vladimir Metodiev</h3>
-                    </MDBContainer>
+                    </div>
                 </MDBContainer>
 
-                <MDBContainer>
-                    <MDBContainer>
+                <MDBContainer className='mt-4'>
+                    <MDBContainer className='mb-4'>
                         <h4>First Name:</h4>
                         <MDBInput label='Vladimir' id='formControlDisabled' type='text' disabled />
                     </MDBContainer>
 
-                    <MDBContainer>
+                    <MDBContainer className='mb-4'>
                         <h4>Last Name:</h4>
                         <MDBInput label='Metodiev' id='formControlDisabled' type='text' disabled />
                     </MDBContainer>
 
-                    <MDBContainer>
+                    <MDBContainer className='mb-4'>
                         <h4>Email:</h4>
                         <MDBInput label='vladimir2907@gmail.com' id='formControlDisabled' type='text' disabled />
                     </MDBContainer>
 
-                    <MDBContainer>
+                    <MDBContainer className='mb-4'>
                         <CustomButton variant='primary'>
                             Account Settings <MdOutlineSettings style={{ fontSize: '17px' }} />
                         </CustomButton>
@@ -62,8 +69,10 @@ const Profile = () => {
 
             <Delimeter theme={theme} />
 
-            <MDBContainer>
-                <h2>Created Cards:</h2>
+            <MDBContainer className='mt-4'>
+                <MDBContainer >
+                    <h2 className='ms-5'>Created Cards:</h2>
+                </MDBContainer>
                 <MDBContainer className='d-flex'>
                     {list}
                 </MDBContainer>
